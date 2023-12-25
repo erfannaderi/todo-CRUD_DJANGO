4750021dd197bb79e93610e924e2925899d35244
erfannaderi <erfan.137979@gmail.com>
2023-12-25 11:58:06 +0330

    fixing edit and adding func to delete btn

--- todoapp/views.py
+++ todoapp/views.py
@@ -36,13 +36,17 @@ class HomeView(ListView):
         return redirect('home')
 
     def edit_task(request, pk):
-        get_task = get_object_or_404(Task, pk=pk)
+        task = get_object_or_404(Task, pk=pk)
         if request.method == 'POST':
-            new_task = get_task
+            # Update the task with the new data from the form
+            task.task = request.POST.get('task')
+            task.save()
+            return redirect('home')  # Redirect to the home page after editing
         else:
-            context = {'get_task': get_task}
-            return render(request, 'edit_task.html', )
+            context = {'task': task}
+            return render(request, 'edit_task.html', context)
 
     def delete_task(request, pk):
-        task = get_object_or_404(Task, pk)
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
         return redirect('home')
